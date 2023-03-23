57eadda8e167cac7ee88f9f20937a86414e3b4de
蒲立望60263 <liwang.pu@going-link.com>
2023-03-23 11:31:40 +0800

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  {
-    path: 'grid-layout',
-    loadChildren: () => import('./grid-layout/grid-layout.module').then(m => m.GridLayoutModule)
-  },
-  { path: '**', redirectTo: 'home' }
+  // { path: 'home', component: HomeComponent },
+  // {
+  //   path: 'grid-layout',
+  //   loadChildren: () => import('./grid-layout/grid-layout.module').then(m => m.GridLayoutModule)
+  // },
+  // { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { ChangeDetectionStrategy, Component, ElementRef } from '@angular/core';
-
-import { customAlphabet } from 'nanoid/non-secure';
-
-const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 8);
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  items: Array<string> = [];
   public constructor(protected el: ElementRef) {
-    for (let idx = 0; idx < 5; idx++) {
-      this.items.push(`${idx}`);
-    }
+
   }
 
-  test(): void {
-    console.log(`el:`, this.el.nativeElement);
-  }
 }
